34e1e3339c7f88f98f6ad41d1ca0a68216eb21e7
francis <francis>
2008-06-03 19:05:30 +0000

    Awesome survey goodness.

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@
 -- Copyright (c) 2008 UK Citizens Online Democracy. All rights reserved.
 -- Email: francis@mysociety.org; WWW: http://www.mysociety.org/
 --
--- $Id: schema.sql,v 1.1 2008-05-27 18:56:40 francis Exp $
+-- $Id: schema.sql,v 1.2 2008-06-03 19:05:32 francis Exp $
 --
 
 -- secret
@@ -14,22 +14,28 @@ create table secret (
     secret text not null
 );
 
-create table data_item (
-    id serial not null primary key,
+-- Records if the survey has been done
+create table survey_done (
+    user_code text not null
+);
+create unique index survey_done_unique_idx on survey_done(user_code);
 
-    user_code text not null,
+-- Stores a row for each piece of info in the survey.
+create table data_item (
+    -- Batch is a random code for the chunk of data.
+    batch text not null,
     site text not null,
 
     key text not null,
     value text not null,
 
-    whenlogged timestamp not null
+    whenstored date not null
 );
 
-create index data_item_user_code_idx on data_item(user_code);
+create index data_item_batch_idx on data_item(batch);
 create index data_item_site_idx on data_item(site);
 create index data_item_key_idx on data_item(key);
 create index data_item_value_idx on data_item(value);
 
-create unique index data_item_unique_idx on data_item(user_code, site, key);
+create unique index data_item_unique_idx on data_item(batch, key);
 
--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@
  * Copyright (c) 2008 UK Citizens Online Democracy. All rights reserved.
  * Email: francis@mysociety.org; WWW: http://www.mysociety.org
  *
- * $Id: index.php,v 1.1 2008-05-27 18:56:41 francis Exp $
+ * $Id: index.php,v 1.2 2008-06-03 19:05:32 francis Exp $
  * 
  */
 
@@ -14,7 +14,7 @@ require_once "../conf/general";
 require_once "../../phplib/error.php";
 require_once "../../phplib/db.php";
 require_once "../../phplib/utility.php";
-require_once "../../phplib/utility.php";
+require_once "../../phplib/auth.php";
 
 function survey_handle_error($num, $message, $file, $line, $context) {
     print("<strong>$message</strong> in $file:$line");
@@ -22,25 +22,53 @@ function survey_handle_error($num, $message, $file, $line, $context) {
 }
 err_set_handler_display('survey_handle_error');
 
-# Get input fields
-$site = $_POST['sourceidentifier'];
-$user_code = "hello"; # XXX
-$signature = $_POST['auth_signature'];
-print "<pre>"; print_r($_POST); print "</pre>";
+if (get_http_var('querydone')) {
+    $user_code = get_http_var('user_code');
+    $signature = get_http_var('auth_signature');
+    $verified = auth_verify_with_shared_secret($user_code, OPTION_SURVEY_SECRET, $signature);
+    if (!$verified)
+        err("Signature wasn't verified.");
+    $already_done = db_getOne('select count(*) from survey_done where user_code = ?', array($user_code));
+    print $already_done ? 1 : 0;
+    return;
+} else {
+    # Get input fields
+    $site = $_POST['sourceidentifier'];
+    $user_code = $_POST['user_code'];
+    $signature = $_POST['auth_signature'];
+    $return_url = $_POST['return_url'];
+    # ... make sure none of those values are logged
+    unset($_POST['user_code']);
+    unset($_POST['auth_signature']);
+    unset($_POST['return_url']);
+    #print "<pre>"; print_r($_POST); print "</pre>";
 
-# Check to see if really from one of our sites
-$verified = auth_verify_with_shared_secret($user_code, OPTION_SURVEY_SECRET, $signature);
-if (!$verified)
-    err("Signature wasn't verified.");
+    # Start transaction
+    db_connect();
 
-# Store the survey record
-db_connect();
-foreach ($_POST as $key => $value) {
-    db_query('insert into data_item (user_code, site, key, value, whenlogged)
-    values (?, ?, ?, ?, now())', array($user_code, $site, $key, $value));
+    # Check to see if really from one of our sites
+    $verified = auth_verify_with_shared_secret($user_code, OPTION_SURVEY_SECRET, $signature);
+    if (!$verified)
+        err("Signature wasn't verified.");
+
+    # See if already there
+    $already_done = db_getOne('select count(*) from survey_done where user_code = ?', array($user_code));
+    if ($already_done) {
+        err("Done already");
+    }
+    db_query('insert into survey_done (user_code) values (?)', array($user_code));
+
+    # Make arbitary key for storing actual data against
+    $batch = auth_random_token();
+
+    # Store the survey record
+    foreach ($_POST as $key => $value) {
+        db_query('insert into data_item (batch, site, key, value, whenstored)
+        values (?, ?, ?, ?, now())', array($batch, $site, $key, $value));
+    }
+    db_commit();
+
+    # Redirect back out
+    header('Location: ' . $return_url);
 }
-db_commit();
-
-#header('Location: ' . $url);
-
 
